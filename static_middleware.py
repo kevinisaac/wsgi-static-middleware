b05ef9e5df8d0135ffa3711013ce91d4eae36c0d
Kevin Isaac <kevin.isaac70@gmail.com>
2015-10-16 17:01:00 +0000

    Added keyword arguments

--- static_middleware.py
+++ static_middleware.py
@@ -1,33 +1,39 @@
 import mimetypes, os
 
-STATIC_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+static_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
 class StaticMiddleware(object):
-    def __init__(self, app):
+    def __init__(self, app, static_dir='', url_prefix='/static'):
         self.app = app
+        self.static_dir = static_dir.strip('/')
+        self.url_prefix = url_prefix
 
     def __call__(self, environ, start_response):
         """
-        WSGI app/middleware to serve files from /static endpoint.
+        WSGI middleware to serve files from /static endpoint.
         """
 
+        # Setting the SCRIPT_NAME
         if not environ['SCRIPT_NAME']:
             environ['SCRIPT_NAME'] = '/' + environ['PATH_INFO'].split('/')[1]
 
-        if environ['SCRIPT_NAME'] != '/static':
+        if environ['SCRIPT_NAME'] != self.url_prefix:
             return self.app(environ, start_response)
         else:
+            # Geterating the full path of the static resource
             path = os.path.abspath(
                 os.path.join(
-                    STATIC_PATH, environ['PATH_INFO'].lstrip('/')
+                    static_path,
+                    self.static_dir,
+                    environ['PATH_INFO'].lstrip('/')
                 )
             )
 
-            if not path.startswith(STATIC_PATH) or not os.path.exists(path):
+            if not path.startswith(static_path) or not os.path.exists(path):
                 start_response("404 not found", [('Content-type', 'text/plain')])
                 return [
                     'File Not Found: %s\n' % environ['PATH_INFO'],
-                    'STATIC_PATH: %s\n' % STATIC_PATH,
+                    'STATIC_PATH: %s\n' % static_path,
                     'Joined path: %s' % path
                 ]
             else:
